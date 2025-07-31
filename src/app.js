cf1fb206dd0b5ba411e5c65588531097b9165ccd
Igor <80968651+webdevbiv@users.noreply.github.com>
2025-08-01 00:21:50 +0300

    FINAL

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
 import express from 'express';
+import { Student } from './models/students.js';
+
 const app = express();
 
-app.get('/students', (req, res) => {
+app.get('/students', async (req, res) => {
+  const students = await Student.find();
   res.json({
     status: 200,
     message: 'Successfully retrieved students',
-    data: [],
+    data: students,
   });
 });
 
-app.get('/students/:id', (req, res) => {
-  const studentId = req.params.id;
-  res.json({
-    status: 200,
-    message: `Successfully retrieved student with ID ${studentId}`,
-    data: {},
-  });
+app.get('/students/:id', async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({
+        status: 404,
+        message: `Student with ID ${req.params.id} not found`,
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully retrieved student with ID ${req.params.id} name ${student.name}`,
+      data: student,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 400,
+      message: `Invalid ID format`,
+    });
+  }
 });
 
 export default app;
